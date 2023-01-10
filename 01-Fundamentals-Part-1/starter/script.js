f67e0bcff3a724e558c23451151f3dd9ea7ba17b
Kayetic <bartek.bak@protonmail.com>
2023-01-10 18:36:21 +0000

    operator precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -68,13 +68,14 @@ job = "teacher";
 
 lastName = "Schmedtmann";
 console.log(lastName);
-*/
 
+
+///////////////////////////////////////
 // Basic Operators
 // Math Operators
 const now = 2037;
 const ageJonas = now - 1991;
-const ageSarah = now - 2018;
+const ageSarah = now - 2020;
 console.log(ageJonas, ageSarah);
 
 console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
@@ -95,3 +96,23 @@ console.log(x);
 
 // Comparison Operators
 console.log(ageJonas > ageSarah); // >, <, >=, <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
+*/
+
+// Operator Precedence
+
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2020;
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10, x = 10
+console.log(x, y);
