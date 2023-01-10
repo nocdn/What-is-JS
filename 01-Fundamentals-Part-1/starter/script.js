d4eee1822add960e300e164f51256d373ee4aefb
Kayetic <bartek.bak@protonmail.com>
2023-01-10 19:31:33 +0000

    if/else statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -101,7 +101,7 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
-*/
+
 
 // Operator Precedence
 
@@ -166,3 +166,25 @@ lines"
 console.log(`String
 multiple
 lines`);
+*/
+
+// Taking Decisions: if / else Statements
+
+const age = 15;
+
+if (age >= 18) {
+  console.log("Sarah can start driving license 🚗");
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 2012;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
