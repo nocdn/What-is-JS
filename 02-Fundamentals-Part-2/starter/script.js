82e789ee445ff1d2806073f948f08a5b6485eeb0
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2023-01-17 09:40:41 +0000

    started functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+/*
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;
+if (hasDriversLicense) console.log("I can drive :D");
+
+// const interface = "Audio";   // reserved keyword
+// const private = 534;         // reserved keyword
+// const if = 23;               // reserved keyword
+*/
+
+// Functions
+function logger() {
+  console.log("My name is Jonas");
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
