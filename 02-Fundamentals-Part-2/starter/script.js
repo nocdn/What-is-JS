6a9d167dae5e5f0a329bce884e13d9de329fd277
Bartek Bak <99417935+Kayetic@users.noreply.github.com>
2023-01-19 14:19:05 +0000

    Functions calling other functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -55,3 +55,17 @@ console.log(age1, age2);
 
 // Arrow function
 const calcAge3 = (birthYear) => 2037 - birthYear;
+const age3 = calcAge3(1991);
+console.log(age3);
+
+const yearsUntilRetirement = (birthYear, firstName) => {
+  const age = 2037 - birthYear;
+  const retirement = 65 - age;
+  // return retirement;
+  return `${firstName} retires in ${retirement} years`;
+};
+
+console.log(yearsUntilRetirement(1991, "Jonas"));
+console.log(yearsUntilRetirement(1980, "Bob"));
+
+// Functions calling other functions
