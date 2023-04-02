9df2d59b910f94e95eb83c2a27a460377ff88af9
Kayetic <bartek.bak@protonmail.com>
2023-04-02 21:27:16 +0100

    🚀 feat(script.js): add jonas object with firstName, lastName, age, job and friends properties

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -228,3 +228,11 @@ const jonasArray = [
   "teacher",
   ["Michael", "Peter", "Steven"],
 ];
+
+const jonas = {
+  firstName: "Jonas",
+  lastName: "Schmedtmann",
+  age: 2037 - 1991,
+  job: "teacher",
+  friends: ["Michael", "Peter", "Steven"],
+};
