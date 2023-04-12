5044178919ac4855caae4e2628e130a45f985fcb
Kayetic <bartek.bak@protonmail.com>
2023-04-12 08:33:31 +0100

    started the developer skills

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,7 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+const x = 23;
+
+//    name       parameter     returns this
+const calcAge = (birthYear) => 2037 - birthYear;
+console.log(calcAge(1991));
