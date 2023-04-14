d385244bc9b1fe829a596f72710419a288d813b8
Kayetic <bartek.bak@protonmail.com>
2023-04-14 01:09:35 +0100

    continuing modifying DOM

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,69 @@
 'use strict';
 
+/*
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = 'Correct Number 🎉';
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+*/
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+console.log(`Number: ${number}`);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  // When no input
+  if (!guess) {
+    document.querySelector('.message').textContent = '🚫 No number!';
+
+    // When player wins
+  } else if (guess === number) {
+    document.querySelector('.message').textContent = 'Correct Number 🎉';
+    document.querySelector('.number').textContent = number;
+
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    // When guess too low
+  } else if (guess < number) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Higher ⬆️';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 😔';
+      document.querySelector('.score').textContent = 0;
+    }
+
+    // When guess too high
+  } else {
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Lower ⬇️';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game! 😔';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('---- GAME RESET ----');
+  number = Math.trunc(Math.random() * 20) + 1;
+  console.log(`New number: ${number}`);
+  let score = 20;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.number').textContent = '?';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').style.width = '15rem';
+  document.querySelector('.guess').value = '';
+});
