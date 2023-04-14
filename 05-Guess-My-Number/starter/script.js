b8f47e346fc3e87c4d5e5f0cfa02d36d6f07399e
Kayetic <bartek.bak@protonmail.com>
2023-04-14 01:21:03 +0100

    started refactoring

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,7 +14,13 @@ console.log(document.querySelector('.guess').value);
 
 let number = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+
 console.log(`Number: ${number}`);
+
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -22,7 +28,8 @@ document.querySelector('.check').addEventListener('click', function () {
 
   // When no input
   if (!guess) {
-    document.querySelector('.message').textContent = '🚫 No number!';
+    // document.querySelector('.message').textContent = '🚫 No number!';
+    displayMessage('🚫 No number!');
 
     // When player wins
   } else if (guess === number) {
@@ -31,6 +38,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
     // When guess too low
   } else if (guess < number) {
     if (score > 1) {
