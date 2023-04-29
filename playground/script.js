f9eb594bb1b04b8331889833d82edb6b696dd0d5
Kayetic <bartek.bak@protonmail.com>
2023-04-29 12:56:47 +0100

    fixed counter not updating, forgot 'let' keyword

--- playground/script.js
+++ playground/script.js
@@ -3,9 +3,9 @@
 const counter = document.querySelector(".counter");
 const button = document.querySelector(".button");
 
-clicked = 0;
+let clicked = 0;
 
 button.addEventListener("click", function () {
   clicked += 1;
-  counter.textContent = "clicked";
+  counter.textContent = "The button has been pressed " + clicked + " times";
 });
