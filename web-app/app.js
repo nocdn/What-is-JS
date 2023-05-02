6f37c61999e307ed9831f0126146f61ab7f1b353
Kayetic <bartek.bak@protonmail.com>
2023-05-02 12:15:02 +0100

    created a timer app

--- web-app/app.js
+++ web-app/app.js
@@ -1,3 +1,59 @@
-// make it so when i press the button in the html code, it changes its text to 'stop'
+let [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+let timerRef = document.querySelector(".timerDisplay");
+let int = null;
+let isRunning = false;
 
-// Path: web-app/app
+const startButton = document.getElementById("startTimer");
+
+document.getElementById("startTimer").addEventListener("click", () => {
+  if (!isRunning) {
+    if (int !== null) {
+      resetTimer();
+    }
+    isRunning = true;
+    int = setInterval(displayTimer, 10);
+    startButton.textContent = "Stop";
+    startButton.style.backgroundColor = "red";
+    startButton.style.color = "white";
+    startButton.style.transition = "all 0.5s ease";
+  } else {
+    isRunning = false;
+    clearInterval(int);
+    console.log(`Elapsed time: ${timerRef.innerHTML}`);
+    startButton.textContent = "Start";
+    startButton.style.backgroundColor = "";
+    startButton.style.color = "";
+  }
+});
+
+function displayTimer() {
+  milliseconds += 10;
+  if (milliseconds == 1000) {
+    milliseconds = 0;
+    seconds++;
+    if (seconds == 60) {
+      seconds = 0;
+      minutes++;
+      if (minutes == 60) {
+        minutes = 0;
+        hours++;
+      }
+    }
+  }
+  let h = hours < 10 ? "0" + hours : hours;
+  let m = minutes < 10 ? "0" + minutes : minutes;
+  let s = seconds < 10 ? "0" + seconds : seconds;
+  let ms =
+    milliseconds < 10
+      ? "00" + milliseconds
+      : milliseconds < 100
+      ? "0" + milliseconds
+      : milliseconds;
+
+  timerRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms}`;
+}
+
+function resetTimer() {
+  [milliseconds, seconds, minutes, hours] = [0, 0, 0, 0];
+  timerRef.innerHTML = "00 : 00 : 00 : 000";
+}
