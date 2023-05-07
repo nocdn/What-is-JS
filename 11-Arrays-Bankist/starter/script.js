1f3987306f5dea2d61f2d5cf17a696205370259b
Kayetic <bartek.bak@protonmail.com>
2023-05-07 01:46:32 +0100

    continued with splitting, splicing and reversing arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -76,3 +76,30 @@ const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 /////////////////////////////////////////////////
 
 let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// SLICE
+console.log(arr.slice(2));
+// start and end index (end not included)
+console.log(arr.slice(2, 4));
+// negative index
+console.log(arr.slice(-2));
+// copy array
+console.log(arr.slice());
+// start from end
+console.log(arr.slice(1, -2));
+
+// SPLICE
+// mutates original array
+
+// console.log(arr.splice(2));
+arr.splice(-1);
+console.log(arr);
+arr.splice(1, 2);
+console.log(arr);
+
+// REVERSE
+// mutates original array
+arr = ['a', 'b', 'c', 'd', 'e'];
+const arr2 = ['j', 'i', 'h', 'g', 'f'];
+console.log(arr2.reverse());
+console.log(arr2);
