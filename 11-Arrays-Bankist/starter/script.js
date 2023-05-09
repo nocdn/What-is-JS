6e932dc44d074ab7ce86ca075ca2560c15546034
Kayetic <bartek.bak@protonmail.com>
2023-05-09 10:08:20 +0100

    continued learning for each looping with arrays

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -4,6 +4,8 @@
 /////////////////////////////////////////////////
 // BANKIST APP
 
+/*
+
 // Data
 const account1 = {
   owner: 'Jonas Schmedtmann',
@@ -103,3 +105,47 @@ arr = ['a', 'b', 'c', 'd', 'e'];
 const arr2 = ['j', 'i', 'h', 'g', 'f'];
 console.log(arr2.reverse());
 console.log(arr2);
+
+// CONCAT
+// does not mutate original array
+const letters = arr.concat(arr2);
+console.log(letters);
+console.log([...arr, ...arr2]);
+
+// JOIN
+// join elements of array into string with a separator
+console.log(letters.join(' - '));
+
+
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+// getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+console.log(arr.at(-1));
+
+console.log(`jonas`.at(0));
+console.log(`jonas`.at(-1));
+
+*/
+
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+for (const movement of movements) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+movements.forEach(function (movement) {
+  if (movement > 0) {
+    console.log(`You deposited ${movement}`);
+  } else {
+    console.log(`You withdrew ${Math.abs(movement)}`);
+  }
+});
